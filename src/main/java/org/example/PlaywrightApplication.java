a957dfaf9be0950f43be50e90af8f56083336126
Bernard Visagie <bernardvisagie@gmail.com>
2023-02-23 13:12:02 +0100

    Fix typo

--- src/main/java/org/example/PlaywrightApplication.java
+++ src/main/java/org/example/PlaywrightApplication.java
@@ -7,7 +7,7 @@ public class PlaywrightApplication {
     public static void main(String[] args) {
         ArrayList<String> arguments = new ArrayList<>(
                 List.of(
-                        "-g", "org.example.playwright.websitesundertest.luma.steps",
+                        "-g", "org.example.playwright.websitesundertest.wikipedia.steps",
                         "-g", "org.example.playwright.configurations",
                         "classpath:features"));
         io.cucumber.core.cli.Main.main(arguments.toArray(String[]::new));
